8e711b4fa6ae56a453ee3473f9ff0873f39b3616
Ask Hylleberg <ash@umbraco.dk>
2024-11-07 13:15:44 +0100

    Adding api service items

---
+++ Api.Service/Controllers/SampleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Service.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SampleController : ControllerBase
+{
+    private readonly ILogger<SampleController> _logger;
+
+    public SampleController(ILogger<SampleController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetSample")]
+    public string Get()
+    {
+        return "HELLOWORLD";
+    }
+}
---
+++ Infrastructure/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure;
+
+public class Constants
+{
+    public static class Service
+    {
+        public const string ServiceName = "SalesPlatform";
+        public const string BoundedContext = "Backend";
+    }
+}
