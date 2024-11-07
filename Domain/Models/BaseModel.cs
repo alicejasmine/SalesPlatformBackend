cc21d597a35eb21a6ea8ac13e866eb3266699e51
Ask Hylleberg <ash@umbraco.dk>
2024-11-07 10:47:55 +0100

    Adding sample models and enums

---
+++ Domain/Models/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models;
+
+public class BaseModel
+{
+    [Key] public Guid Id { get; set; }
+
+    public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime Modified { get; set; }
+}
---
+++ Domain/Models/SampleModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Models
+{
+    public class SampleModel : BaseModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    }
+}
