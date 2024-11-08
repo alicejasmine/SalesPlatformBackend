eaa10c19f25b240f180d2cf1ea4d6f513c2099c6
Alice Salvi <82236372+alicejasmine@users.noreply.github.com>
2024-11-08 14:22:28 +0100

    refactoring in program.cs

---
+++ ApplicationServices/ServiceCollectionExtension.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationServices.Sample;
+using Infrastructure;
+using Infrastructure.Repository.Sample;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApplicationServices;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<ISampleService, SampleService>();
+        
+       return services;
+    }
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<ISampleRepository, SampleRepository>();
+       
+        return services;
+    }
+    public static IServiceCollection AddDbContext(this IServiceCollection services)
+    {
+        string connectionString = Environment.GetEnvironmentVariable("sqlconn")
+                                  ?? throw new InvalidOperationException("Database connection string not set.");
+
+        services.AddDbContext<SalesPlatformDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        return services;
+    }
+}
