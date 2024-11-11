700f51644e38b89fe465b6818207022fa7de5ee0
FaNzu <ask.hylleberg@gmail.com>
2024-11-11 13:25:03 +0100

    #5 setup tests

---
+++ Integration.Tests/ConfigurationTestFixture.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Integration.Tests;
+
+[TestFixture]
+public abstract class ConfigurationTestFixture
+{
+    protected IConfiguration Configuration { get; private set; }
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        Configuration = GetConfiguration();
+
+        DoSetup();
+    }
+
+    private IConfiguration GetConfiguration()
+    {
+        var configurationBuilder = new ConfigurationBuilder()
+            .AddEnvironmentVariables();
+
+        configurationBuilder.AddEnvironmentVariables();
+
+
+        Configure(configurationBuilder);
+
+        return configurationBuilder.Build();
+    }
+
+    protected virtual void Configure(IConfigurationBuilder configure) { }
+
+    public virtual void DoSetup() { }
+}
---
+++ Integration.Tests/Endpoints/Sample/SampleControllerEndpointTests.cs
@@ -0,0 +1,100 @@
+﻿using Domain.Sample;
+using System.Collections.Immutable;
+using System.Net;
+using System.Net.Http.Json;
+using Umbraco.Cloud.Usage.Environment.Integration.Tests.Endpoints;
+
+namespace Integration.Tests.Endpoints.Sample;
+
+[TestFixture]
+public sealed class SampleControllerEndpointTests : BaseEndpointTest
+{
+    private const string BaseUrl = "/Sample";
+
+    [Test]
+    public async Task GetSample_ShouldReturnSample_WhenSampleExists()
+    {
+        // Arrange
+        var sampleId = Guid.NewGuid();
+        await SeedSampleAsync(sampleId);
+
+        // Act
+        var response = await Client.GetAsync($"{BaseUrl}/GetSample?id={sampleId}");
+
+        // Assert
+        var sample = await response.Content.ReadFromJsonAsync<SampleModel>();
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.That(sample, Is.Not.Null);
+        Assert.That(sample.Id, Is.EqualTo(sampleId));
+    }
+
+    [Test]
+    public async Task GetAllSamples_ShouldReturnAllSamples()
+    {
+        // Arrange
+        await SeedSamplesAsync();
+
+        // Act
+        var response = await Client.GetAsync($"{BaseUrl}/GetAllSample");
+
+        // Assert
+        var samples = await response.Content.ReadFromJsonAsync<ImmutableHashSet<SampleDto>>();
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.That(samples, Is.Not.Empty);
+    }
+
+    [Test]
+    public async Task CreateSample_ShouldCreateSample_WhenModelIsValid()
+    {
+        // Arrange
+        var sample = new SampleDto(Guid.NewGuid(), "Sample Name", "Description", 100, DateTime.Now, DateTime.Now);
+
+        // Act
+        var response = await Client.PostAsJsonAsync($"{BaseUrl}/CreateSample", sample);
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+
+    [Test]
+    public async Task DeleteSample_ShouldReturnOk_WhenSampleExists()
+    {
+        // Arrange
+        var sampleId = Guid.NewGuid();
+        await SeedSampleAsync(sampleId);
+
+        // Act
+        var response = await Client.DeleteAsync($"{BaseUrl}/DeleteSample?id={sampleId}");
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+
+    [Test]
+    public async Task DeleteSample_ShouldReturnBadRequest_WhenSampleDoesNotExist()
+    {
+        // Arrange
+        var nonExistentSampleId = Guid.NewGuid();
+
+        // Act
+        var response = await Client.DeleteAsync($"{BaseUrl}/DeleteSample?id={nonExistentSampleId}");
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+    }
+
+    private async Task SeedSampleAsync(Guid sampleId)
+    {
+        var sample = new SampleDto(Guid.NewGuid(), "Sample Name", "Description", 100, DateTime.Now, DateTime.Now);
+
+        await Client.PostAsJsonAsync($"{BaseUrl}/CreateSample", sample);
+    }
+
+    private async Task SeedSamplesAsync()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            await SeedSampleAsync(Guid.NewGuid());
+        }
+    }
+}
