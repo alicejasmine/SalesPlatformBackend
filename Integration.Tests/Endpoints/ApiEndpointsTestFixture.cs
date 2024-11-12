871e3deff1dd6c11b0edf767bf5f1ccc83c11570
FaNzu <ask.hylleberg@gmail.com>
2024-11-12 11:20:40 +0100

    made connection to the docker, tests need configuration

--- Integration.Tests/Endpoints/ApiEndpointsTestFixture.cs
+++ Integration.Tests/Endpoints/ApiEndpointsTestFixture.cs
@@ -1,11 +1,13 @@
 ﻿using ApplicationServices.Sample;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Moq;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure;
+using Testcontainers.MsSql;
 
 namespace Integration.Tests.Endpoints
 {
@@ -13,18 +15,37 @@ namespace Integration.Tests.Endpoints
     {
         protected HttpClient Client { get; private set; }
         private WebApplicationFactory<Program> _factory;
+        private MsSqlContainer _sqlContainer;
+
+        [OneTimeSetUp]
+        public async Task SettingUpSql()
+        {
+            _sqlContainer = new MsSqlBuilder()
+                .WithImage("mcr.microsoft.com/mssql/server:2019-latest")
+                .WithPassword("YourStrongPassword123!")
+                .Build();
+
+            await _sqlContainer.StartAsync();
+        }
 
         [SetUp]
         public void SetUp()
         {
+            var connectionString = _sqlContainer.GetConnectionString();
+
+            Environment.SetEnvironmentVariable("sqlconn", connectionString);
+
             _factory = new WebApplicationFactory<Program>()
             .WithWebHostBuilder(builder =>
             {
                 builder.ConfigureServices(services =>
                 {
+                    services.RemoveAll<DbContextOptions<SalesPlatformDbContext>>();
+                    services.AddDbContext<SalesPlatformDbContext>(options =>
+                        options.UseSqlServer(connectionString));
+
                     services.RemoveAll<ISampleService>();
                     var mockSampleService = new Mock<ISampleService>();
-
                     services.AddSingleton(mockSampleService.Object);
                 });
 
@@ -38,16 +59,27 @@ namespace Integration.Tests.Endpoints
             Client = _factory.CreateClient();
         }
 
-        [TearDown]
-        public void Dispose()
+        [OneTimeTearDown]
+        public async Task DisposeAsync()
         {
             Client?.Dispose();
             _factory?.Dispose();
+
+            if (_sqlContainer is not null)
+            {
+                await _sqlContainer.StopAsync();
+                await _sqlContainer.DisposeAsync();
+            }
         }
 
         protected T GetService<T>() where T : notnull
         {
             return _factory.Services.GetRequiredService<T>();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Integration.Tests/SqlDbTestFixture.cs
+++ Integration.Tests/SqlDbTestFixture.cs
@@ -1,41 +1,41 @@
-﻿using Infrastructure;
-using Microsoft.EntityFrameworkCore;
+﻿//using Infrastructure;
+//using Microsoft.EntityFrameworkCore;
 
-namespace Integration.Tests
-{
-    public abstract class SqlDbTestFixture : ConfigurationTestFixture
-    {
-        protected DbContextOptions<SalesPlatformDbContext> DbContextOptions;
-        protected SalesPlatformDbContext DbContext;
+//namespace Integration.Tests
+//{
+//    public abstract class SqlDbTestFixture : ConfigurationTestFixture
+//    {
+//        protected DbContextOptions<SalesPlatformDbContext> DbContextOptions;
+//        protected SalesPlatformDbContext DbContext;
 
-        [SetUp]
-        public void Setup()
-        {
-            DbContextOptions = new DbContextOptionsBuilder<SalesPlatformDbContext>()
-                .UseInMemoryDatabase("TestDatabase")
-                .Options;
+//        [SetUp]
+//        public void Setup()
+//        {
+//            DbContextOptions = new DbContextOptionsBuilder<SalesPlatformDbContext>()
+//                .UseInMemoryDatabase("TestDatabase")
+//                .Options;
 
-            DbContext = new SalesPlatformDbContext(DbContextOptions);
-            DbContext.Database.EnsureDeleted();
-            DbContext.Database.EnsureCreated();
+//            DbContext = new SalesPlatformDbContext(DbContextOptions);
+//            DbContext.Database.EnsureDeleted();
+//            DbContext.Database.EnsureCreated();
 
-            ConfigureDatabase();
-            DoSetup();
-        }
+//            ConfigureDatabase();
+//            DoSetup();
+//        }
 
-        [TearDown]
-        public void Teardown()
-        {
-            DbContext?.Dispose();
-            DoTeardown();
-        }
+//        [TearDown]
+//        public void Teardown()
+//        {
+//            DbContext?.Dispose();
+//            DoTeardown();
+//        }
 
-        private void ConfigureDatabase()
-        {
-            // Add seed data or initial setup if needed
-        }
+//        private void ConfigureDatabase()
+//        {
+//            // Add seed data or initial setup if needed
+//        }
 
-        public virtual Task DoSetup() { return Task.CompletedTask; }
-        public virtual Task DoTeardown() { return Task.CompletedTask; }
-    }
-}
+//        public virtual Task DoSetup() { return Task.CompletedTask; }
+//        public virtual Task DoTeardown() { return Task.CompletedTask; }
+//    }
+//}
