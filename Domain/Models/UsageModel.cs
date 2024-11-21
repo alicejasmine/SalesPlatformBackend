d82a689eb49cfc034d9d4d83d5df5b894d7f0c5e
Alice Salvi <82236372+alicejasmine@users.noreply.github.com>
2024-11-21 09:59:36 +0100

    add usage model and daily usage model

---
+++ Domain/Models/UsageModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Models;
+
+public class UsageModel
+{
+    public string DocumentId { get; init; }
+    public Guid PartitionKey { get; init; }
+    public Guid ProjectId { get; init; }
+    public Guid EnvironmentId { get; init; }
+    public DateOnly CreationDate { get; init; }
+    public long TotalMonthlyBandwidth { get; set; }
+    public long TotalMonthlyMedia { get; set; }
+    public Dictionary<DateOnly, DailyUsageModel> Days { get; init; } = new Dictionary<DateOnly, DailyUsageModel>();
+}
