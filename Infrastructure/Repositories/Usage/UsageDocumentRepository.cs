a04ddb260ecc1102789f46530a8b7612f3dab67f
Alice Salvi <82236372+alicejasmine@users.noreply.github.com>
2024-11-21 10:38:12 +0100

    add logger to repository

--- Infrastructure/Repositories/Usage/UsageDocumentRepository.cs
+++ Infrastructure/Repositories/Usage/UsageDocumentRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Infrastructure.Repositories.Usage;
+﻿using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Repositories.Usage;
 
 public class UsageDocumentRepository  : IUsageDocumentRepository
 {
-    
+    private readonly ILogger<UsageDocumentRepository> _logger;
+    public UsageDocumentRepository(ILogger<UsageDocumentRepository> logger)
+    {
+        _logger = logger;
+    }
 }
