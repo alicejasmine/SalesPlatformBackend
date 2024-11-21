5b7da009eb57bc5628e7ba6db8d628717f627943
Alice Salvi <82236372+alicejasmine@users.noreply.github.com>
2024-11-21 14:31:09 +0100

    starting with cosmosDb setup for tests

--- Infrastructure/Constants.cs
+++ Infrastructure/Constants.cs
@@ -7,4 +7,11 @@ public class Constants
         public const string ServiceName = "SalesPlatform";
         public const string BoundedContext = "Backend";
     }
+    
+    public static class CosmosDbProperties
+    {
+        public const string PartitionKeyPath = "/PartitionKey";
+        public const string CollectionName = "Usages";
+        public const string TestDatabaseName = "TestUsage";
+    }
 }
---
+++ Integration.Tests/CosmosDbTestFixture.cs
@@ -0,0 +1,67 @@
+﻿using Infrastructure;
+using Microsoft.Azure.Cosmos;
+
+
+namespace Integration.Tests;
+
+public abstract class CosmosDbTestFixture
+{
+    protected CosmosClient CosmosDbClient;
+
+    [SetUp]
+    public async Task Setup()
+    {
+        var cosmosConnection = GetCosmosDbConnection();
+
+        CosmosDbClient = new CosmosClient(cosmosConnection.ConnectionString, new CosmosClientOptions
+        {
+            ConnectionMode = ConnectionMode.Gateway,
+            HttpClientFactory = () => cosmosConnection.client,
+        });
+
+        await ConfigureTestDatabase();
+        await DoSetup();
+    }
+
+    [TearDown]
+    public async Task Teardown()
+    {
+        await UsageTestContainer.DeleteContainerAsync();
+
+        DoTeardown();
+
+        if (CosmosDbClient == null)
+            return;
+
+        CosmosDbClient.Dispose();
+        CosmosDbClient = null;
+    }
+    
+    private static (HttpClient client, string ConnectionString) GetCosmosDbConnection()
+    {
+        var endpoint = Environment.GetEnvironmentVariable("CosmosDbServiceEndpoint");
+        var authkey = Environment.GetEnvironmentVariable("CosmosDbServiceAuthKey");
+        
+        if (string.IsNullOrEmpty(endpoint) || string.IsNullOrEmpty(authkey))
+        {
+            throw new InvalidOperationException("Cosmos DB connection details are missing in the configuration");
+        }
+
+        var connectionString = $"AccountEndpoint={endpoint};AccountKey={authkey}";
+        var client = new HttpClient();
+
+        return (client, connectionString);
+    }
+
+    private async Task ConfigureTestDatabase()
+    {
+        var databaseResponse = await CosmosDbClient.CreateDatabaseIfNotExistsAsync(Constants.CosmosDbProperties.TestDatabaseName, 1000);
+        var containerProperties = new ContainerProperties(Constants.CosmosDbProperties.CollectionName, Constants.CosmosDbProperties.PartitionKeyPath);
+        await databaseResponse.Database.CreateContainerIfNotExistsAsync(containerProperties);
+    }
+    
+    protected Container UsageTestContainer => CosmosDbClient.GetContainer(Constants.CosmosDbProperties.TestDatabaseName, Constants.CosmosDbProperties.CollectionName);
+
+    public virtual Task DoSetup() { return Task.CompletedTask; }
+    public virtual Task DoTeardown() { return Task.CompletedTask; }
+}
