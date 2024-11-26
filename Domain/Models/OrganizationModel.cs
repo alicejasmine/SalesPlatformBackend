50c5acaf74cc12cad8aaa02e97db4ca858d12020
Ask Hylleberg <ash@umbraco.dk>
2024-11-26 11:09:03 +0100

    first example overview of all models

---
+++ Domain/Models/OrganizationModel.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Enum;
+
+namespace Domain.Models;
+
+public class OrganizationModel : BaseModel
+{
+    public OrganizationModel(Guid id, string alias, string displayName, string description, int totalCredits, PartnershipEnum partnership, ICollection<ProjectModel> projects, ICollection<PurchaseModel> purchases, ICollection<CreditHistoryModel> creditHistory, DateTime created, DateTime modified)
+        : base(id, created, modified)
+    {
+        Alias = alias;
+        DisplayName = displayName;
+        Description = description;
+        TotalCredits = totalCredits;
+        Partnership = partnership;
+        Projects = projects ?? new List<ProjectModel>();
+        Purchases = purchases ?? new List<PurchaseModel>();
+        CreditHistory = creditHistory ?? new List<CreditHistoryModel>();
+    }
+
+    public string Alias { get; set; }
+    public string DisplayName { get; set; }
+    public string Description { get; set; }
+    public int TotalCredits { get; private set; }
+    public PartnershipEnum Partnership { get; set; }
+    public ICollection<ProjectModel> Projects { get; set; }
+    public ICollection<PurchaseModel> Purchases { get; set; }
+    public ICollection<CreditHistoryModel> CreditHistory { get; set; }
+}
---
+++ Domain/Models/ProjectModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Models;
+
+public class ProjectModel : BaseModel
+{
+    public ProjectModel(Guid id, string displayName, string alias, DateTime created, DateTime modified, PlanModel plan, Guid organizationId)
+            : base(id, created, modified)
+    {
+        EnvironmentId = id;
+        DisplayName = displayName;
+        Alias = alias;
+        Plan = plan;
+        OrganizationId = organizationId;
+    }
+
+    public Guid EnvironmentId { get; set; }
+    public string DisplayName { get; set; }
+    public string Alias { get; set; }
+    public PlanModel Plan { get; set; }
+    public Guid OrganizationId { get; set; }
+}
