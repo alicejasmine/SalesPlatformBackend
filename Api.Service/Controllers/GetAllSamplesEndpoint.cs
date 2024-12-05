6d4ccc37f3f412fd8f1dbfe8c276124f4faa6566
Alice Salvi <82236372+alicejasmine@users.noreply.github.com>
2024-12-05 15:13:16 +0100

    GetAllSamplesEndpoint with Ardalis

---
+++ Api.Service/Controllers/GetAllSamplesEndpoint.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Immutable;
+using ApplicationServices.Sample;
+using Ardalis.ApiEndpoints;
+using Domain.Sample;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Api.Service.Controllers;
+
+public class GetAllSamplesEndpoint : EndpointBaseAsync.WithoutRequest.WithActionResult<ImmutableHashSet<SampleModel>>
+{
+    private readonly ISampleService _sampleService;
+
+    public GetAllSamplesEndpoint(ISampleService sampleService)
+    {
+        _sampleService = sampleService;
+    }
+
+    [HttpGet("GetAllSamples")]
+    [ProducesResponseType(typeof(ImmutableHashSet<SampleModel>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    [SwaggerOperation(
+        Summary = "Get All Samples",
+        Description = "Retrieve all samples as an immutable set",
+        OperationId = "GetAllSamples",
+        Tags = new[] { "Sample" })
+    ]
+    public override async Task<ActionResult<ImmutableHashSet<SampleModel>>> HandleAsync(
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        try
+        {
+            var samples = await _sampleService.GetAllSamplesAsync();
+            return Ok(samples.ToImmutableHashSet());
+        }
+        catch (Exception ex)
+        {
+            return Problem(
+                title: "Error Retrieving Samples",
+                detail: ex.Message,
+                statusCode: 500
+            );
+        }
+    }
+}
