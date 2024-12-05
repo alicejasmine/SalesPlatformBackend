02efea91aab98fe611c26c797921a61f3eddda26
Ask Hylleberg <ash@umbraco.dk>
2024-12-05 12:29:03 +0100

    adding name to plan

--- Domain/Entities/PlanEntity.cs
+++ Domain/Entities/PlanEntity.cs
@@ -3,6 +3,7 @@
 namespace Domain.Entities;
 public class PlanEntity : BaseEntity
 {
+    public string Name { get; set; }
     public PlanEnum Plan { get; set; }
     public int PriceInDKK { get; set; }
 }
