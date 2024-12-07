ad242416d0350a63a95d386b633b4952f144f0db
Alice Salvi <82236372+alicejasmine@users.noreply.github.com>
2024-12-07 15:48:10 +0100

    add endpoint

---
+++ Api.Service/Project/DTOs/GetProjectByAliasRequestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Api.Service.Project.DTOs;
+
+public class GetProjectByAliasRequestDto
+{
+    public string Alias { get; set; }
+}
---
+++ Api.Service/Project/ProjectMapper.cs
@@ -0,0 +1,22 @@
+﻿using Api.Service.Project.DTOs;
+using Domain.Models;
+
+namespace Api.Service.Project;
+
+public static class ProjectMapper
+{
+    public static ProjectResponse MapModelToResponse(ProjectModel project)
+    {
+        return new ProjectResponse
+        {
+            Id = project.Id,
+            DisplayName = project.DisplayName,
+            Alias = project.Alias,
+            EnvironmentId = project.EnvironmentId,
+            PlanId = project.PlanId,
+            OrganizationId = project.OrganizationId,
+            Created = project.Created,
+            Modified = project.Modified
+        };
+    }
+}
