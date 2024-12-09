47c581f466f410896f58f6bd5fbdfeb75b9356d6
Ask Hylleberg <ash@umbraco.dk>
2024-12-09 14:22:54 +0100

    adding tests for sample service

--- Integration.Tests/Sample/SampleRepositoryTests.cs
+++ Integration.Tests/Sample/SampleRepositoryTests.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure.Repository.Sample;
+﻿using Domain.Sample;
+using Infrastructure.Repository.Sample;
 using Integration.Tests.Library;
 using Test.Fixtures.Sample;
 
@@ -20,21 +21,34 @@ public sealed class SampleRepositoryTests : BaseDatabaseTestFixture
     {
         // Arrange
         var sampleEntity = SampleModelFixture.DefaultSample;
+        var anotherSampleEntity = new SampleModelBuilder()
+            .WithId(Guid.NewGuid())
+            .WithName("Another Sample")
+            .WithDescription("Another Description")
+            .WithPrice(1)
+            .Build();
 
         await _sampleRepository.UpsertAsync(sampleEntity);
+        await _sampleRepository.UpsertAsync(anotherSampleEntity);
 
         // Act
         var samples = await _sampleRepository.GetAllSamplesAsync();
 
         // Assert
         Assert.That(samples, Is.Not.Empty);
-        var firstSample = samples.First();
-        Assert.That(firstSample.Id, Is.EqualTo(SampleModelFixture.DefaultSample.Id));
-        Assert.That(firstSample.Description, Is.EqualTo(SampleModelFixture.DefaultSample.Description));
-        Assert.That(firstSample.Name, Is.EqualTo(SampleModelFixture.DefaultSample.Name));
-        Assert.That(firstSample.Price, Is.EqualTo(SampleModelFixture.DefaultSample.Price));
+        Assert.That(samples.Count(), Is.EqualTo(2));
     }
 
+    [Test]
+    public async Task GetAllSamples_ShouldReturnEmpty_WhenNoSamplesExist()
+    {
+        // Act
+        var samples = await _sampleRepository.GetAllSamplesAsync();
+
+        // Assert
+        Assert.That(samples, Is.Empty);
+    }
+    
     [Test]
     public async Task GetSampleById_ShouldReturnCorrectSample()
     {
@@ -49,8 +63,8 @@ public sealed class SampleRepositoryTests : BaseDatabaseTestFixture
         // Assert
         Assert.That(sample, Is.Not.Null);
         Assert.That(sample.Id, Is.EqualTo(SampleModelFixture.DefaultSample.Id));
-        Assert.That(sample.Description, Is.EqualTo(SampleModelFixture.DefaultSample.Description));
         Assert.That(sample.Name, Is.EqualTo(SampleModelFixture.DefaultSample.Name));
+        Assert.That(sample.Description, Is.EqualTo(SampleModelFixture.DefaultSample.Description));
         Assert.That(sample.Price, Is.EqualTo(SampleModelFixture.DefaultSample.Price));
     }
 
@@ -67,13 +81,65 @@ public sealed class SampleRepositoryTests : BaseDatabaseTestFixture
         Assert.That(sample, Is.Null);
     }
 
+
     [Test]
-    public async Task GetAllSamples_ShouldReturnEmpty_WhenNoSamplesExist()
+    public async Task UpsertAsync_ShouldInsertNewSample()
     {
+        // Arrange
+        var sampleEntity = SampleModelFixture.DefaultSample;
+
         // Act
-        var samples = await _sampleRepository.GetAllSamplesAsync();
+        var insertedSample = await _sampleRepository.UpsertAsync(sampleEntity);
 
         // Assert
-        Assert.That(samples, Is.Empty);
+        Assert.That(DatabaseTestsFixture.DbContext.SampleEntities.Count, Is.EqualTo(1));
+        Assert.That(insertedSample, Is.Not.Null);
+        Assert.That(insertedSample.Id, Is.EqualTo(SampleModelFixture.DefaultSample.Id));
+        Assert.That(insertedSample.Name, Is.EqualTo(SampleModelFixture.DefaultSample.Name));
+        Assert.That(insertedSample.Description, Is.EqualTo(SampleModelFixture.DefaultSample.Description));
+        Assert.That(insertedSample.Price, Is.EqualTo(SampleModelFixture.DefaultSample.Price));
+    }
+
+    [Test]
+    public async Task UpsertAsync_ShouldUpdateExistingSample()
+    {
+        // Arrange
+        var sampleEntity = SampleModelFixture.DefaultSample;
+        var insertedSample = await _sampleRepository.UpsertAsync(sampleEntity);
+
+        Assert.That(DatabaseTestsFixture.DbContext.SampleEntities.Count, Is.EqualTo(1));
+        insertedSample.Name = "Updated Product Name";
+
+        // Act
+        var updatedSample = await _sampleRepository.UpsertAsync(insertedSample);
+
+        // Assert
+        Assert.That(updatedSample, Is.Not.Null);
+        Assert.That(updatedSample.Name, Is.EqualTo("Updated Product Name"));
+    }
+
+    [Test]
+    public async Task DeleteAsync_ShouldRemoveSample()
+    {
+        // Arrange
+        var sampleEntity = SampleModelFixture.DefaultSample;
+        await _sampleRepository.UpsertAsync(sampleEntity);
+
+        // Act
+        await _sampleRepository.DeleteAsync(sampleEntity.Id);
+
+        // Assert
+        var sample = await _sampleRepository.GetByIdAsync(sampleEntity.Id);
+        Assert.That(sample, Is.Null);
+    }
+
+    [Test]
+    public void DeleteAsync_ShouldThrowException_WhenSampleDoesNotExist()
+    {
+        // Arrange
+        var nonExistingId = Guid.NewGuid();
+
+        // Act & Assert
+        Assert.ThrowsAsync<ArgumentException>(() => _sampleRepository.DeleteAsync(nonExistingId));
     }
 }
