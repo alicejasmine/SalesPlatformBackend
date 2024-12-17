89c9c69deab1862d77b87e989b683eea17e31ea2
Alice Salvi <82236372+alicejasmine@users.noreply.github.com>
2024-12-17 19:05:33 +0100

    update models to include credits

--- Domain/Models/CreditHistoryModel.cs
+++ Domain/Models/CreditHistoryModel.cs
@@ -2,21 +2,24 @@
 
 public class CreditHistoryModel : BaseModel
 {
-    public CreditHistoryModel(Guid id, DateTime created, DateTime modified, int invoiceNumber, int partnershipCredits, int creditsSpend, int newTotalCredits, DateOnly creditStart, DateOnly creditEnd)
+    public CreditHistoryModel(Guid id, DateTime created, DateTime modified, int invoiceNumber, int partnershipCredits, 
+        int creditsSpend, int currentCredits, DateOnly creditStart, DateOnly creditEnd, Guid organizationId)
         : base(id, created, modified)
     {
         InvoiceNumber = invoiceNumber;
         PartnershipCredits = partnershipCredits;
         CreditsSpend = creditsSpend;
-        NewTotalCredits = newTotalCredits;
+        CurrentCredits = currentCredits;
         CreditStart = creditStart;
         CreditEnd = creditEnd;
+        OrganizationId = organizationId;
     }
 
     public int InvoiceNumber { get; set; }
     public int PartnershipCredits { get; set; }
     public int CreditsSpend { get; set; }
-    public int NewTotalCredits { get; set; }
+    public int CurrentCredits { get; set; }
     public DateOnly CreditStart { get; set; }
     public DateOnly CreditEnd { get; set; }
+    public Guid OrganizationId { get; set; } 
 }
