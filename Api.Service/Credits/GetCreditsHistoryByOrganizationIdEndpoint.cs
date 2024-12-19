b13890b9dd6a28e8d3bf4a057abfa43ec731067b
Alice Salvi <82236372+alicejasmine@users.noreply.github.com>
2024-12-19 10:35:24 +0100

    update endpoint to use FromQuery

--- Api.Service/Credits/GetCreditsHistoryByOrganizationIdEndpoint.cs
+++ Api.Service/Credits/GetCreditsHistoryByOrganizationIdEndpoint.cs
@@ -6,7 +6,7 @@ using Swashbuckle.AspNetCore.Annotations;
 
 namespace Api.Service.Credits;
 
-public class GetCreditsHistoryByOrganizationAliasEndpoint : EndpointBaseAsync.WithRequest<string>.WithActionResult<CreditHistoryResponse>
+public class GetCreditsHistoryByOrganizationAliasEndpoint : EndpointBaseAsync.WithRequest<GetCreditsHistoryByOrganizationAliasRequestDto>.WithActionResult<List<CreditHistoryResponse>>
 {
     private readonly ICreditService _creditService;
 
@@ -15,7 +15,7 @@ public class GetCreditsHistoryByOrganizationAliasEndpoint : EndpointBaseAsync.Wi
         _creditService = creditService;
     }
 
-    [HttpGet("GetCreditsHistoryByAlias/{organizationAlias}")]
+    [HttpGet("GetCreditsHistoryByOrganizationAlias")]
     [ProducesResponseType(typeof(CreditHistoryResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
@@ -24,17 +24,17 @@ public class GetCreditsHistoryByOrganizationAliasEndpoint : EndpointBaseAsync.Wi
         Description = "Retrieve the credit history for a specific organization based on its alias",
         OperationId = "GetCreditsHistoryByOrganizationAlias")
     ]
-    public override async Task<ActionResult<CreditHistoryResponse>> HandleAsync(string organizationAlias, CancellationToken cancellationToken = new CancellationToken())
+    public override async Task<ActionResult<List<CreditHistoryResponse>>> HandleAsync([FromQuery] GetCreditsHistoryByOrganizationAliasRequestDto dto, CancellationToken cancellationToken = new CancellationToken())
     {
         try
         {
-            var creditHistories = await _creditService.GetCreditHistoryByOrganizationAlias(organizationAlias);
+            var creditHistories = await _creditService.GetCreditHistoryByOrganizationAlias(dto.OrganizationAlias);
 
             if (creditHistories == null)
             {
                 return Problem(
                     title: "Organization Not Found",
-                    detail: $"No credit history found for the organization with alias '{organizationAlias}'.",
+                    detail: $"No credit history found for the organization with alias '{dto.OrganizationAlias}'.",
                     statusCode: 404
                 );
             }
@@ -43,7 +43,7 @@ public class GetCreditsHistoryByOrganizationAliasEndpoint : EndpointBaseAsync.Wi
             {
                 InvoiceNumber=ch.InvoiceNumber,
                 OrganizationId = ch.OrganizationId,
-                OrganizationAlias = organizationAlias,
+                OrganizationAlias = dto.OrganizationAlias,
                 TotalPartnershipCredits = ch.PartnershipCredits,
                 CreditsSpent = ch.CreditsSpend,
                 RemainingCredits = ch.CurrentCredits,
